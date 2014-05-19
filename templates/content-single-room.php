f8e063fac1acb716403f57ba9371db1a79093d1a
Tareq Hasan <tareq1988@gmail.com>
2014-05-19 14:30:42 +0600

    contents moved from content to single

--- templates/content-single-room.php
+++ templates/content-single-room.php
@@ -1,65 +1,29 @@
-<div class="bediq-container">
+<article itemscope itemtype="http://schema.org/Product" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-    <?php
-    /**
-     * bediq_template_post_title, 10
-     * bediq_template_room_thumbnail_image, 15
-     */
-    do_action( 'bediq_before_single_room' ); ?>
-
-    <div class="bediq-col-9">
-
-        <article itemscope itemtype="http://schema.org/Product" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-            <div class="bediq-summary-before">
-                <?php
-                /**
-                 * bediq_template_room_schema, 15
-                 * bediq_template_room_book_now, 20
-                 */
-                do_action( 'bediq_before_single_room_summary' ); ?>
-            </div>
-
-            <div class="bediq-summary">
-
-                <?php
-                /**
-                 * bediq_template_post_content, 10
-                 */
-                do_action( 'bediq_single_room_summary' ); ?>
-            </div>
-
-            <div class="bediq-summary-after">
-                <?php
-                /**
-                 * bediq_template_room_tab_room 15
-                 */
-                do_action( 'bediq_after_single_room_summary' ); ?>
-            </div>
-
-        </article>
+    <div class="bediq-summary-before">
+        <?php
+        /**
+         * bediq_template_room_schema, 15
+         * bediq_template_room_book_now, 20
+         */
+        do_action( 'bediq_before_single_room_summary' ); ?>
     </div>
 
-    <div class="bediq-col-3 sidebar">
+    <div class="bediq-summary">
 
-        <aside class="room-nav-widget">
-            <?php
-                $current_room = get_the_ID();
-                $rooms = new WP_Query('post_type=bediq_room&posts_per_page=-1');
-            ?>
-            <ul class="room-nav">
+        <?php
+        /**
+         * bediq_template_post_content, 10
+         */
+        do_action( 'bediq_single_room_summary' ); ?>
+    </div>
 
-                <?php while ( $rooms->have_posts() ) : $rooms->the_post(); ?>
-                <?php
-                    $class = (get_the_ID() == $current_room ? 'current-room' : '');
-                ?>
-                <li><a class="<?php echo $class; ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-                <?php endwhile; wp_reset_postdata(); ?>
-            </ul>
-        </aside>
+    <div class="bediq-summary-after">
+        <?php
+        /**
+         * bediq_template_room_tab_room 15
+         */
+        do_action( 'bediq_after_single_room_summary' ); ?>
+    </div>
 
-        <?php dynamic_sidebar( 'room-sidebar' ); ?>
-
-    </div> <!-- .bediq-col-3 .sidebar -->
-
-</div> <!-- .bediq-container -->
+</article>
--- templates/single-bediq_room.php
+++ templates/single-bediq_room.php
@@ -15,11 +15,48 @@ get_header( 'bediq' ); ?>
 
 	<?php do_action( 'bediq_before_main_content' ); ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+    <div class="bediq-container">
 
-			<?php bediq_get_template_part( 'content', 'single-room' ); ?>
+        <?php
+        /**
+         * bediq_template_post_title, 10
+         * bediq_template_room_thumbnail_image, 15
+         */
+        do_action( 'bediq_before_single_room' ); ?>
 
-		<?php endwhile; // end of the loop. ?>
+        <div class="bediq-col-9">
+
+    		<?php while ( have_posts() ) : the_post(); ?>
+
+    			<?php bediq_get_template_part( 'content', 'single-room' ); ?>
+
+    		<?php endwhile; // end of the loop. ?>
+        </div>
+
+
+        <div class="bediq-col-3 sidebar">
+
+            <aside class="room-nav-widget">
+                <?php
+                    $current_room = get_the_ID();
+                    $rooms = new WP_Query('post_type=bediq_room&posts_per_page=-1');
+                ?>
+                <ul class="room-nav">
+
+                    <?php while ( $rooms->have_posts() ) : $rooms->the_post(); ?>
+                    <?php
+                        $class = (get_the_ID() == $current_room ? 'current-room' : '');
+                    ?>
+                    <li><a class="<?php echo $class; ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                    <?php endwhile; wp_reset_postdata(); ?>
+                </ul>
+            </aside>
+
+            <?php dynamic_sidebar( 'room-sidebar' ); ?>
+
+        </div> <!-- .bediq-col-3 .sidebar -->
+
+    </div>
 
 	<?php do_action( 'bediq_after_main_content' ); ?>
 
