e77b159bdf32cd5d5ad2806ab730579390d1d7e6
Tareq Hasan <tareq1988@gmail.com>
2014-05-19 14:24:34 +0600

    hook name fixes

--- templates/content-archive-room.php
+++ templates/content-archive-room.php
@@ -1,16 +1,13 @@
-<?php
-/**
- * bediq_template_room_book_now - 10
- */
-do_action( 'bediq_before_archive_room' ); ?>
+<?php do_action( 'bediq_before_archive_room' ); ?>
 
     <article itemscope itemtype="http://schema.org/Product" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
         <?php
         /**
-         * bediq_template_featured_image - 10
-         * bediq_template_archive_post_title - 15
-         * bediq_template_post_content - 20
+         * @hooked bediq_template_featured_image - 10
+         * @hooked bediq_template_archive_post_title - 15
+         * @hooked bediq_template_room_book_now - 20
+         * @hooked bediq_template_post_content - 25
          */
         do_action( 'bediq_archive_room' ); ?>
 
